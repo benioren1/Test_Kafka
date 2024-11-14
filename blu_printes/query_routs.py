60e7b19e9add6745f1fe74d026862bcd2d060ed5
beni oren <benioren123beni@gmail.com>
2024-11-14 16:16:05 +0200

    i finish the project and i did function query for find the world the meny tims

--- blu_printes/query_routs.py
+++ blu_printes/query_routs.py
@@ -1,5 +1,6 @@
 from flask import Blueprint , request
 from pymongo import MongoClient
+from collections import Counter
 
 from db import session
 from models.explos_model import Explos
@@ -10,9 +11,6 @@ MONGO_URI = 'mongodb://localhost:27017/'
 DB_NAME = 'all_email'
 
 COLLECTION_NAME = 'all_messages'
-
-
-
 
 def get_collection():
     client = MongoClient(MONGO_URI)
@@ -45,8 +43,35 @@ def get_all_messages_by_email():
 @bp_query.route("/by_froud_email", methods=["GET"])
 def get_all_messages_by_forged_email():
     data = request.get_json()
-    all_ho = session.query(Hostage).filter(email=data['email'])
+    email1 = data['email']
+    all_ho = session.query(Hostage).filter_by(email=email1)
     messages_ho = [message.to_dict() for message in all_ho]
-    all_ex = session.query(Explos).filter(email=data['email'])
+    all_ex = session.query(Explos).filter_by(email=email1)
     messages_ex = [message.to_dict() for message in all_ex]
     return jsonify({"hostage_messages": messages_ho, "explos_messages": messages_ex}), 200
+
+
+
+
+
+@bp_query.route("/world_by_email", methods=["GET"])
+def get_all_messages_by_hostage_name():
+    data = request.get_json()
+    email = data["email"]
+    my_dict = {}
+    all_ho = session.query(Hostage).filter_by(email=email)
+    messages_ho = [message.to_dict() for message in all_ho]
+    all_ex = session.query(Explos).filter_by(email=email)
+    messages_ex = [message.to_dict() for message in all_ex]
+    all_messages = messages_ex + messages_ho
+    sentences = [message['sentences'] for message in all_messages]
+    tst = " ".join(sentences)
+    cleaned_text = tst.replace('.', ' ').replace(',', ' ').replace(';', ' ').replace('!', ' ').replace('?', ' ')
+    words = cleaned_text.split()
+    for letter in words:
+        if letter not in my_dict:
+            my_dict[letter] = 0
+        my_dict[letter] += 1
+    return jsonify({"world_messages": my_dict}), 200
+
+
